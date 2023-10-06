299e1ee8cb53b94e9cc75f95edc9d534da8a3946
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-06 19:38:01 -0300

    added error handling for admins api

--- src/main/java/com/onfleet/api/AdministratorApi.java
+++ src/main/java/com/onfleet/api/AdministratorApi.java
@@ -1,5 +1,7 @@
 package com.onfleet.api;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.onfleet.exceptions.ApiException;
 import com.onfleet.models.Administrator;
 import com.onfleet.utils.HttpMethodType;
 import com.onfleet.utils.JsonUtils;
@@ -8,34 +10,44 @@ import okhttp3.OkHttpClient;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import java.util.ArrayList;
+
 public class AdministratorApi extends ApiBase {
 	public AdministratorApi(OkHttpClient client) {
 		super(client, "/admins");
 	}
 
-	public void createAdministrator(Administrator admin) throws Exception {
+	public Administrator createAdministrator(Administrator admin) throws Exception {
 		String jsonPayload = JsonUtils.toJson(admin);
 		RequestBody body = RequestBody.create(jsonPayload, MediaTypes.JSON);
 		Response response = sendRequest(HttpMethodType.POST, body, baseUrl);
-		System.out.println(response.body().string());
+		if (!response.isSuccessful()) {
+			throw new ApiException(response);
+		}
+		return JsonUtils.fromJson(response.body().string(), Administrator.class);
 	}
 
-	public void listAdministrators() throws Exception {
+	public ArrayList<Administrator> listAdministrators() throws Exception {
 		Response response = sendRequest(HttpMethodType.GET, baseUrl);
-		System.out.println(response.body().string());
+		if (!response.isSuccessful()) {
+			throw new ApiException(response);
+		}
+		return JsonUtils.fromJson(response.body().string(), new TypeReference<ArrayList<Administrator>>() {});
 	}
 
-	public void updateAdministrator(String adminId, Administrator admin) throws Exception {
+	public Administrator updateAdministrator(String adminId, Administrator admin) throws Exception {
 		String url = String.format("%s/%s", baseUrl, adminId);
 		String jsonPayload = JsonUtils.toJson(admin);
 		RequestBody body = RequestBody.create(jsonPayload, MediaTypes.JSON);
 		Response response = sendRequest(HttpMethodType.PUT, body, url);
-		System.out.println(response.body().string());
+		if (!response.isSuccessful()) {
+			throw new ApiException(response);
+		}
+		return JsonUtils.fromJson(response.body().string(), Administrator.class);
 	}
 
 	public void deleteAdministrator(String adminId) throws Exception {
 		String url = String.format("%s/%s", baseUrl, adminId);
-		Response response = sendRequest(HttpMethodType.DELETE, url);
-		System.out.println(response.body().string());
+		sendRequest(HttpMethodType.DELETE, url);
 	}
 }
