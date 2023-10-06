5ba160af3d0bff43bc014788903bcb7fba15dd97
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-06 16:09:38 -0300

    refactored destination api class

--- src/main/java/com/onfleet/api/DestinationApi.java
+++ src/main/java/com/onfleet/api/DestinationApi.java
@@ -1,42 +1,30 @@
-package com.onfleet.destinations;
+package com.onfleet.api;
 
-import com.onfleet.OnFleet;
 import com.onfleet.models.Destination;
+import com.onfleet.utils.HttpMethodType;
 import com.onfleet.utils.JsonUtils;
 import com.onfleet.utils.MediaTypes;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
 //TODO: improve error handling
-public class DestinationApi {
-	private final String URL = OnFleet.URL + "/destinations";
-	private final OkHttpClient client;
+public class DestinationApi extends ApiBase {
 
 	public DestinationApi(OkHttpClient client) {
-		this.client = client;
+		super(client, "/destinations");
 	}
 
 	public void createDestination(Destination destination) throws Exception {
 		String jsonPayload = JsonUtils.toJson(destination);
 		RequestBody body = RequestBody.create(jsonPayload, MediaTypes.JSON);
-		Request request = new Request.Builder()
-				.url(URL)
-				.post(body)
-				.build();
-		Response response = client.newCall(request).execute();
+		Response response = sendRequest(HttpMethodType.POST, body, baseUrl);
 		System.out.println(response.body().string());
-
 	}
 
 	public void getDestination(String id) throws Exception {
-		String url = String.format("%s/%s", URL, id);
-		Request request = new Request.Builder()
-				.url(url)
-				.get()
-				.build();
-		Response response = client.newCall(request).execute();
+		String url = String.format("%s/%s", baseUrl, id);
+		Response response = sendRequest(HttpMethodType.GET, url);
 		System.out.println(response.body().string());
 	}
 }
