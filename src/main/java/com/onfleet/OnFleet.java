09697bbba84239061e2d50e3009b4c903ed4be12
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-06 16:09:51 -0300

    refactored project structure

--- src/main/java/com/onfleet/OnFleet.java
+++ src/main/java/com/onfleet/OnFleet.java
@@ -1,8 +1,9 @@
 package com.onfleet;
 
-import com.onfleet.containers.ContainerApi;
-import com.onfleet.destinations.DestinationApi;
-import com.onfleet.tasks.TaskApi;
+import com.onfleet.api.AdministratorApi;
+import com.onfleet.api.ContainerApi;
+import com.onfleet.api.DestinationApi;
+import com.onfleet.api.TaskApi;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
@@ -10,23 +11,18 @@ import java.util.Base64;
 
 public class OnFleet {
 	public static final String URL = "https://onfleet.com/api/v2";
+	private OkHttpClient client;
 	private final TaskApi taskApi;
 	private final DestinationApi destinationApi;
 	private final ContainerApi containerApi;
+	private final AdministratorApi administratorApi;
 
 	public OnFleet(String apiKey) {
-		String credentials = Base64.getEncoder().encodeToString((apiKey).getBytes());
-		OkHttpClient httpClient = new OkHttpClient.Builder()
-				.addInterceptor(chain -> {
-					Request originalRequest = chain.request();
-					Request newRequest = originalRequest.newBuilder()
-							.header("Authorization", "Basic " + credentials)
-							.build();
-					return chain.proceed(newRequest);
-				}).build();
-		taskApi = new TaskApi(httpClient);
-		destinationApi = new DestinationApi(httpClient);
-		containerApi = new ContainerApi(httpClient);
+		initClient(apiKey);
+		taskApi = new TaskApi(client);
+		destinationApi = new DestinationApi(client);
+		containerApi = new ContainerApi(client);
+		administratorApi = new AdministratorApi(client);
 	}
 
 	public TaskApi getTaskApi() {
@@ -39,6 +35,22 @@ public class OnFleet {
 
 	public ContainerApi getContainerApi() {
 		return containerApi;
+	}
+
+	public AdministratorApi getAdministratorApi() {
+		return administratorApi;
+	}
+
+	private void initClient(String apiKey) {
+		String credentials = Base64.getEncoder().encodeToString((apiKey).getBytes());
+		this.client = new OkHttpClient.Builder()
+				.addInterceptor(chain -> {
+					Request originalRequest = chain.request();
+					Request newRequest = originalRequest.newBuilder()
+							.header("Authorization", "Basic " + credentials)
+							.build();
+					return chain.proceed(newRequest);
+				}).build();
 	}
 
 }
