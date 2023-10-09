5e3d1551776818ee38138d8c1c8eae82d6008e28
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-09 14:59:40 -0300

    changed ApiBase response handling

--- src/main/java/com/onfleet/api/ApiBase.java
+++ src/main/java/com/onfleet/api/ApiBase.java
@@ -1,11 +1,17 @@
 package com.onfleet.api;
 
 import com.onfleet.OnFleet;
+import com.onfleet.exceptions.ApiException;
+import com.onfleet.utils.GsonSingleton;
 import com.onfleet.utils.HttpMethodType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 
 public class ApiBase {
 	protected final String baseUrl;
@@ -16,29 +22,56 @@ public class ApiBase {
 		this.baseUrl = OnFleet.URL + endpoint;
 	}
 
-	protected Response sendRequest(HttpMethodType method, RequestBody body, String url) throws Exception {
-		Request.Builder requestBuilder = new Request.Builder().url(url);
-		switch (method) {
-			case GET:
-				requestBuilder.get();
-				break;
-			case POST:
-				requestBuilder.post(body);
-				break;
-			case PUT:
-				requestBuilder.put(body);
-				break;
-			case DELETE:
-				requestBuilder.delete();
-				break;
-			default:
-				throw new IllegalArgumentException("Invalid HTTP method: " + method);
+	protected Response sendRequest(HttpMethodType method, RequestBody body, String url) throws ApiException {
+		try {
+			Request.Builder requestBuilder = new Request.Builder().url(url);
+			switch (method) {
+				case GET:
+					requestBuilder.get();
+					break;
+				case POST:
+					requestBuilder.post(body);
+					break;
+				case PUT:
+					requestBuilder.put(body);
+					break;
+				case DELETE:
+					requestBuilder.delete();
+					break;
+				default:
+					throw new IllegalArgumentException("Invalid HTTP method: " + method);
+			}
+			Request request = requestBuilder.build();
+			Response response = client.newCall(request).execute();
+			if (!response.isSuccessful()) {
+				throw new ApiException(response);
+			}
+			return response;
+		} catch (IOException e) {
+			throw new ApiException("Failed to send HTTP request: " + e.getMessage(), e);
 		}
-		Request request = requestBuilder.build();
-		return client.newCall(request).execute();
 	}
 
-	protected Response sendRequest(HttpMethodType method, String url) throws Exception {
+	protected Response sendRequest(HttpMethodType method, String url) throws ApiException {
 		return sendRequest(method, null, url);
 	}
+
+	protected <T> T handleResponse(Response response, Type type) throws ApiException {
+		if (!response.isSuccessful()) {
+			throw new ApiException(response);
+		}
+		ResponseBody responseBody = response.body();
+		if (responseBody != null) {
+			try {
+				String json = responseBody.string();
+				return GsonSingleton.getInstance().fromJson(json, type);
+			} catch (IOException e) {
+				throw new ApiException("Failed to read response body: " + e.getMessage(), e);
+			} finally {
+				responseBody.close();
+			}
+		}
+		return null;
+	}
+
 }
