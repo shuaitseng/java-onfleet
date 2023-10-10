29dbc9b99b9a44fd944f0daf3566995705860756
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-10 17:47:22 -0300

    added model and changed method return type

---
+++ src/main/java/com/onfleet/models/WorkerRoute.java
@@ -0,0 +1,34 @@
+package com.onfleet.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class WokerRoute {
+	private String workerId;
+	@JsonProperty("vehicle")
+	private VehicleType vehicleType;
+	private RouteStep steps;
+
+	public String getWorkerId() {
+		return workerId;
+	}
+
+	public void setWorkerId(String workerId) {
+		this.workerId = workerId;
+	}
+
+	public VehicleType getVehicleType() {
+		return vehicleType;
+	}
+
+	public void setVehicleType(VehicleType vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+
+	public RouteStep getSteps() {
+		return steps;
+	}
+
+	public void setSteps(RouteStep steps) {
+		this.steps = steps;
+	}
+}
