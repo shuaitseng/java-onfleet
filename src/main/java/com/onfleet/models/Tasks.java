1fc350e71988a80737ae58e1562bb4e5a27899ec
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-10 17:56:19 -0300

    removed spaces between lines

--- src/main/java/com/onfleet/models/Tasks.java
+++ src/main/java/com/onfleet/models/Tasks.java
@@ -4,13 +4,10 @@ import java.util.List;
 
 public class Tasks {
 	private List<Task> tasks;
-
 	public Tasks() {}
-
 	public List<Task> getTasks() {
 		return tasks;
 	}
-
 	public void setTasks(List<Task> tasks) {
 		this.tasks = tasks;
 	}
