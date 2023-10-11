ed962a7cb946ce740f494520c1debd980a5b70ff
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-11 17:04:23 -0300

    added base test class to remove code duplication

---
+++ src/test/java/com/onfleet/api/BaseApiTest.java
@@ -0,0 +1,32 @@
+package com.onfleet.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.lang.reflect.Field;
+
+public class BaseApiTest {
+	protected MockWebServer mockWebServer;
+	protected OkHttpClient okHttpClient;
+
+	@BeforeEach
+	public void setUpBase() throws Exception {
+		okHttpClient = new OkHttpClient();
+		mockWebServer = new MockWebServer();
+		mockWebServer.start();
+	}
+
+	@AfterEach
+	public void tearDownBase() throws Exception {
+		mockWebServer.shutdown();
+	}
+
+	protected void setBaseUrl(BaseApi api, String baseUrl) throws Exception {
+		Field baseUrlField = api.getClass().getSuperclass().getDeclaredField("baseUrl");
+		baseUrlField.setAccessible(true);
+		baseUrlField.set(api, baseUrl);
+	}
+
+}
