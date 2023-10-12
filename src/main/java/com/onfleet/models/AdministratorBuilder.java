78ff98ab35e551262b650640d9ebcc7b03b8858f
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-12 18:17:08 -0300

    added builder for admin

---
+++ src/main/java/com/onfleet/models/AdministratorBuilder.java
@@ -0,0 +1,38 @@
+package com.onfleet.models;
+
+public class AdministratorBuilder {
+	private String name;
+	private String email;
+	private String phone;
+	private Boolean isReadOnly;
+	private Administrator.Type type;
+
+	public AdministratorBuilder setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public AdministratorBuilder setEmail(String email) {
+		this.email = email;
+		return this;
+	}
+
+	public AdministratorBuilder setPhone(String phone) {
+		this.phone = phone;
+		return this;
+	}
+
+	public AdministratorBuilder setIsReadOnly(Boolean isReadOnly) {
+		this.isReadOnly = isReadOnly;
+		return this;
+	}
+
+	public AdministratorBuilder setType(Administrator.Type type) {
+		this.type = type;
+		return this;
+	}
+
+	public Administrator build() {
+		return new Administrator(name, email, phone, isReadOnly, type);
+	}
+}
