4c2d131026d00fc26e44385b335ef823e0d96992
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-12 12:00:04 -0300

    added remaining api classes

--- src/main/java/com/onfleet/OnFleet.java
+++ src/main/java/com/onfleet/OnFleet.java
@@ -9,45 +9,69 @@ import java.util.Base64;
 public class OnFleet {
 	public static final String URL = "https://onfleet.com/api/v2";
 	private OkHttpClient client;
-	private final TaskApi taskApi;
-	private final DestinationApi destinationApi;
-	private final ContainerApi containerApi;
-	private final AdministratorApi administratorApi;
 	private final OrganizationApi organizationApi;
+	private final AdministratorApi administratorApi;
+	private final WorkerApi workerApi;
 	private final HubApi hubApi;
+	private final TeamApi teamApi;
+	private final DestinationApi destinationApi;
+	private final RecipientApi recipientApi;
+	private final TaskApi taskApi;
+	private final ContainerApi containerApi;
+	private final WebhookApi webhookApi;
 
 	public OnFleet(String apiKey) {
 		initClient(apiKey);
-		taskApi = new TaskApi(client);
-		destinationApi = new DestinationApi(client);
-		containerApi = new ContainerApi(client);
-		administratorApi = new AdministratorApi(client);
 		organizationApi = new OrganizationApi(client);
+		administratorApi = new AdministratorApi(client);
+		workerApi = new WorkerApi(client);
 		hubApi = new HubApi(client);
-	}
-
-	public TaskApi getTaskApi() {
-		return taskApi;
-	}
-
-	public DestinationApi getDestinationApi() {
-		return destinationApi;
-	}
-
-	public ContainerApi getContainerApi() {
-		return containerApi;
-	}
-
-	public AdministratorApi getAdministratorApi() {
-		return administratorApi;
+		teamApi = new TeamApi(client);
+		destinationApi = new DestinationApi(client);
+		recipientApi = new RecipientApi(client);
+		taskApi = new TaskApi(client);
+		containerApi = new ContainerApi(client);
+		webhookApi = new WebhookApi(client);
 	}
 
 	public OrganizationApi getOrganizationApi() {
 		return organizationApi;
 	}
 
+	public AdministratorApi getAdministratorApi() {
+		return administratorApi;
+	}
+
+	public WorkerApi getWorkerApi() {
+		return workerApi;
+	}
+
 	public HubApi getHubApi() {
 		return hubApi;
+	}
+
+	public TeamApi getTeamApi() {
+		return teamApi;
+	}
+
+	public DestinationApi getDestinationApi() {
+		return destinationApi;
+	}
+
+	public RecipientApi getRecipientApi() {
+		return recipientApi;
+	}
+
+	public TaskApi getTaskApi() {
+		return taskApi;
+	}
+
+	public ContainerApi getContainerApi() {
+		return containerApi;
+	}
+
+	public WebhookApi getWebhookApi() {
+		return webhookApi;
 	}
 
 	private void initClient(String apiKey) {
