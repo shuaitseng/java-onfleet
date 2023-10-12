caba67004cab072cd6ca4912959d724ce2822a3f
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-12 18:19:00 -0300

    added missing test cases

--- src/test/java/com/onfleet/api/AdministratorApiTest.java
+++ src/test/java/com/onfleet/api/AdministratorApiTest.java
@@ -3,6 +3,7 @@ package com.onfleet.api;
 import com.google.gson.reflect.TypeToken;
 import com.onfleet.exceptions.ApiException;
 import com.onfleet.models.Administrator;
+import com.onfleet.models.AdministratorBuilder;
 import com.onfleet.models.ErrorResponse;
 import com.onfleet.utils.GsonSingleton;
 import com.onfleet.utils.HttpMethodType;
@@ -36,7 +37,7 @@ class AdministratorApiTest extends BaseApiTest {
 		MockResponse mockResponse = new MockResponse()
 				.setResponseCode(HttpURLConnection.HTTP_OK)
 				.setBody(mockResponseJson);
-		Administrator administrator = new Administrator("name", "cm@onf.lt", "8885551234", true, Administrator.Type.SUPER);
+		Administrator administrator = new AdministratorBuilder().setName("name").setEmail("cm@onf.lt").setPhone("8885551234").setIsReadOnly(true).setType(Administrator.Type.SUPER).build();
 
 		mockWebServer.enqueue(mockResponse);
 		Administrator admin = administratorApi.createAdministrator(administrator);
@@ -56,7 +57,7 @@ class AdministratorApiTest extends BaseApiTest {
 				.setBody(mockResponseJson);
 		mockWebServer.enqueue(mockResponse);
 
-		Administrator administrator = new Administrator("name", "cm@onf.lt", "8885551234", true, Administrator.Type.SUPER);
+		Administrator administrator = new AdministratorBuilder().setName("name").setEmail("cm@onf.lt").setPhone("8885551234").setIsReadOnly(true).setType(Administrator.Type.SUPER).build();
 		ApiException apiException = assertThrows(ApiException.class, () -> administratorApi.createAdministrator(administrator));
 		RecordedRequest recordedRequest = mockWebServer.takeRequest();
 
@@ -87,28 +88,73 @@ class AdministratorApiTest extends BaseApiTest {
 	}
 
 	@Test
-	void testUpdateAdmin() {
-		String mockResponseJson = "{\"id\": \"8AxaiKwMd~np7I*YP2NfukBE\", \"timeCreated\": 1455156651000, \"timeLastModified\": 1455156651779, \"organization\": \"yAM*fDkztrT3gUcz9mNDgNOL\", \"email\": \"cm@onf.lt\", \"type\": \"standard\", \"name\": \"Chelsea M\", \"isActive\": false, \"metadata\": [],\"isAccountOwner\": false, \"teams\": []}";
+	void testUpdateAdmin() throws Exception {
+		String mockResponseJson = "{\"id\":\"8AxaiKwMd~np7I*YP2NfukBE\",\"timeCreated\":1455156651000,\"timeLastModified\":1455156652494,\"organization\":\"yAM*fDkztrT3gUcz9mNDgNOL\",\"email\":\"cm@onf.lt\",\"type\":\"standard\",\"name\":\"C Manning\",\"isActive\":false,\"metadata\":[]}";
 		MockResponse mockResponse = new MockResponse()
 				.setResponseCode(HttpURLConnection.HTTP_OK)
 				.setBody(mockResponseJson);
 		mockWebServer.enqueue(mockResponse);
 
+		Administrator admin = new AdministratorBuilder().setName("New Admin super").setType(Administrator.Type.STANDARD).build();
+		admin = administratorApi.updateAdministrator("8AxaiKwMd~np7I*YP2NfukBE", admin);
+		RecordedRequest recordedRequest = mockWebServer.takeRequest();
+
+		assertEquals(HttpMethodType.PUT.name(), recordedRequest.getMethod());
+		assertEquals("/admins/8AxaiKwMd~np7I*YP2NfukBE", recordedRequest.getPath());
+		Assertions.assertThat(admin)
+				.usingRecursiveComparison()
+				.isEqualTo(GsonSingleton.getInstance().fromJson(mockResponseJson, Administrator.class));
 	}
 
 	@Test
-	void testUpdateInvalidAdminId() {
+	void testUpdateInvalidAdminId() throws Exception {
+		String mockResponseJson = "{\"code\":\"InvalidContent\",\"message\":{\"error\":1005,\"message\":\"The data types of one or more parameters are invalid.\",\"cause\":\"PuLjIsI8nF1xGU3vRWn2XA~Ta must be of type ObjectId\",\"request\":\"aa7d0676-5f2d-4baa-aa38-4285c2bdb438\"}}";
+		MockResponse mockResponse = new MockResponse()
+				.setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST)
+				.setBody(mockResponseJson);
+		mockWebServer.enqueue(mockResponse);
 
+		Administrator admin = new AdministratorBuilder().setName("New Admin super").setType(Administrator.Type.STANDARD).build();
+		ApiException exception = assertThrows(ApiException.class, () -> administratorApi.updateAdministrator("8AxaiKwMd~np7I*YP2NfukBE", admin));
+		RecordedRequest recordedRequest = mockWebServer.takeRequest();
+
+		assertEquals(HttpMethodType.PUT.name(), recordedRequest.getMethod());
+		assertEquals("/admins/8AxaiKwMd~np7I*YP2NfukBE", recordedRequest.getPath());
+		Assertions.assertThat(exception.getErrorResponse())
+				.usingRecursiveComparison()
+				.isEqualTo(GsonSingleton.getInstance().fromJson(mockResponseJson, ErrorResponse.class));
 	}
 
 	@Test
-	void testUpdateAdminIdNonExistent() {
+	void testUpdateAdminIdNonExistent() throws Exception {
+		String mockResponseJson = "{\"code\":\"ResourceNotFound\",\"message\":{\"error\":1402,\"message\":\"The requested resource does not exist.\",\"request\":\"a9212a19-73f9-461a-ae76-cfa1da474d4e\"}}";
+		MockResponse mockResponse = new MockResponse()
+				.setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST)
+				.setBody(mockResponseJson);
+		mockWebServer.enqueue(mockResponse);
 
+		Administrator admin = new AdministratorBuilder().setName("New Admin super").setType(Administrator.Type.STANDARD).build();
+		ApiException exception = assertThrows(ApiException.class, () -> administratorApi.updateAdministrator("8AxaiKwMd~np7I*YP2NfukBE", admin));
+		RecordedRequest recordedRequest = mockWebServer.takeRequest();
+
+		assertEquals(HttpMethodType.PUT.name(), recordedRequest.getMethod());
+		assertEquals("/admins/8AxaiKwMd~np7I*YP2NfukBE", recordedRequest.getPath());
+		Assertions.assertThat(exception.getErrorResponse())
+				.usingRecursiveComparison()
+				.isEqualTo(GsonSingleton.getInstance().fromJson(mockResponseJson, ErrorResponse.class));
 	}
 
 	@Test
-	void testDeleteAdmin() {
+	void testDeleteAdmin() throws Exception {
+		MockResponse mockResponse = new MockResponse()
+				.setResponseCode(HttpURLConnection.HTTP_OK);
+		mockWebServer.enqueue(mockResponse);
 
+		administratorApi.deleteAdministrator("8AxaiKwMd~np7I*YP2NfukBE");
+		RecordedRequest recordedRequest = mockWebServer.takeRequest();
+
+		assertEquals(HttpMethodType.DELETE.name(), recordedRequest.getMethod());
+		assertEquals("/admins/8AxaiKwMd~np7I*YP2NfukBE", recordedRequest.getPath());
 	}
 
 	@Test
