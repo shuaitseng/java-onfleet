d706dd02c4ee742b31a6e5b93c5e9b83a0e7442f
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-12 20:01:20 -0300

    added getters for Vehicle class

--- src/main/java/com/onfleet/models/Vehicle.java
+++ src/main/java/com/onfleet/models/Vehicle.java
@@ -5,4 +5,43 @@ public class Vehicle {
 	private String description;
 	private String licensePlate;
 	private String color;
+
+	public Vehicle(VehicleType type, String description, String licensePlate, String color) {
+		this.type = type.getValue();
+		this.description = description;
+		this.licensePlate = licensePlate;
+		this.color = color;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+
+	public void setLicensePlate(String licensePlate) {
+		this.licensePlate = licensePlate;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
 }
