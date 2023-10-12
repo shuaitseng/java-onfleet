8a7db29901b5e28b3127b4c4d7b05c050e0512d3
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-12 18:17:25 -0300

    fixed breaking null value in admins types

--- src/main/java/com/onfleet/utils/AdminTypeAdapter.java
+++ src/main/java/com/onfleet/utils/AdminTypeAdapter.java
@@ -2,6 +2,7 @@ package com.onfleet.utils;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import com.onfleet.models.Administrator;
 
@@ -11,11 +12,19 @@ public class AdminTypeAdapter extends TypeAdapter<Administrator.Type> {
 
 	@Override
 	public void write(JsonWriter jsonWriter, Administrator.Type type) throws IOException {
+		if (type == null) {
+			jsonWriter.nullValue();
+			return;
+		}
 		jsonWriter.value(type.getStringValue().toLowerCase());
 	}
 
 	@Override
 	public Administrator.Type read(JsonReader jsonReader) throws IOException {
+		if (jsonReader.peek() == JsonToken.NULL) {
+			jsonReader.nextNull();
+			return null;
+		}
 		String lowerCase = jsonReader.nextString().toLowerCase();
 		for (Administrator.Type type : Administrator.Type.values()) {
 			if (type.getStringValue().equals(lowerCase)) {
