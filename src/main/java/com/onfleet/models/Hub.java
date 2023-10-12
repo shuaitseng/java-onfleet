81806b57bc93e06c3be05fb49dde059439f88807
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-12 13:01:03 -0300

    removed unnecessary annotations and added missing getters and setters

--- src/main/java/com/onfleet/models/Hub.java
+++ src/main/java/com/onfleet/models/Hub.java
@@ -6,19 +6,39 @@ import java.util.List;
 
 public class Hub {
 	private String id;
-	@JsonProperty("name")
 	private String name;
 	private List<Double> location;
-	@JsonProperty("address")
 	private Address address;
-	@JsonProperty("team")
 	private List<String> team;
 
-	// TODO: reorder constructor parameters
 	public Hub(String name, Address address, List<String> team) {
 		this.name = name;
 		this.address = address;
 		this.team = team;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Double> getLocation() {
+		return location;
+	}
+
+	public void setLocation(List<Double> location) {
+		this.location = location;
 	}
 
 	public Address getAddress() {
@@ -35,13 +55,5 @@ public class Hub {
 
 	public void setTeam(List<String> team) {
 		this.team = team;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 }
