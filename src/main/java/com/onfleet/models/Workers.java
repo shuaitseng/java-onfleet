c6a5c784e84e371c52264982f14d6e497e9ccc81
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-13 16:48:29 -0300

    added class to map workers response

---
+++ src/main/java/com/onfleet/models/Workers.java
@@ -0,0 +1,15 @@
+package com.onfleet.models;
+
+import java.util.List;
+
+public class Workers {
+	private List<Worker> workers;
+
+	public List<Worker> getWorkers() {
+		return workers;
+	}
+
+	public void setWorkers(List<Worker> workers) {
+		this.workers = workers;
+	}
+}
