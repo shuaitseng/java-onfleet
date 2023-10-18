51136ae9bc4c2eb4f330fcd879371aa3d4928636
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-18 18:28:12 -0300

    switched Organization for Delegatee

--- src/main/java/com/onfleet/api/OrganizationApi.java
+++ src/main/java/com/onfleet/api/OrganizationApi.java
@@ -1,7 +1,8 @@
 package com.onfleet.api;
 
 import com.onfleet.exceptions.ApiException;
-import com.onfleet.models.Organization;
+import com.onfleet.models.organization.Delegatee;
+import com.onfleet.models.organization.Organization;
 import com.onfleet.utils.HttpMethodType;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
@@ -17,9 +18,9 @@ public class OrganizationApi extends BaseApi {
 		return handleResponse(response, Organization.class);
 	}
 
-	public Organization getDelegateeDetails(String id) throws ApiException {
+	public Delegatee getDelegateeDetails(String id) throws ApiException {
 		String url = String.format("%s/%s", baseUrl + "s", id);
 		Response response = sendRequest(HttpMethodType.GET, url);
-		return handleResponse(response, Organization.class);
+		return handleResponse(response, Delegatee.class);
 	}
 }
