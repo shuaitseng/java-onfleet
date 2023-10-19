66f4d11ace8d093a14d90f13e86b32e3c4668de9
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-19 12:35:42 -0300

    finished worker methods

--- src/main/java/com/onfleet/api/WorkerApi.java
+++ src/main/java/com/onfleet/api/WorkerApi.java
@@ -4,12 +4,18 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import com.onfleet.exceptions.ApiException;
-import com.onfleet.models.ScheduleEntries;
-import com.onfleet.models.ScheduleEntry;
-import com.onfleet.models.task.Tasks;
-import com.onfleet.models.Worker;
+import com.onfleet.models.Metadata;
 import com.onfleet.models.WorkerFilterFields;
 import com.onfleet.models.Workers;
+import com.onfleet.models.worker.Worker;
+import com.onfleet.models.worker.WorkerCreateParams;
+import com.onfleet.models.worker.WorkerListQueryParams;
+import com.onfleet.models.worker.WorkerQueryParams;
+import com.onfleet.models.worker.WorkerScheduleEntries;
+import com.onfleet.models.worker.WorkerScheduleEntry;
+import com.onfleet.models.worker.WorkerTasks;
+import com.onfleet.models.worker.WorkerTasksQueryParams;
+import com.onfleet.models.worker.WorkerUpdateParams;
 import com.onfleet.utils.GsonSingleton;
 import com.onfleet.utils.HttpMethodType;
 import com.onfleet.utils.MediaTypes;
@@ -27,35 +33,31 @@ public class WorkerApi extends BaseApi {
 		super(client, "/workers");
 	}
 
-	public Worker createWorker(Worker worker) throws ApiException {
+	public Worker createWorker(WorkerCreateParams worker) throws ApiException {
 		String jsonPayload = GsonSingleton.getInstance().toJson(worker);
 		RequestBody body = RequestBody.create(jsonPayload, MediaTypes.JSON);
 		Response response = sendRequest(HttpMethodType.POST, body, baseUrl);
 		return handleResponse(response, Worker.class);
 	}
 
-	public List<Worker> listWorkers(List<String> fields,
-	                                List<String> teamIds,
-	                                List<Integer> workerStates,
-	                                List<String> phoneNumbers,
-	                                boolean includePasswordDetails) throws ApiException {
+	public List<Worker> listWorkers(WorkerListQueryParams params) throws ApiException {
 		HttpUrl.Builder urlBuilder = HttpUrl.parse(baseUrl).newBuilder();
-		if (fields != null && !fields.isEmpty()) {
-			urlBuilder.addQueryParameter("fields", String.join(",", fields));
+		if (params.getFields() != null && !params.getFields().isEmpty()) {
+			urlBuilder.addQueryParameter("fields", String.join(",", params.getFields()));
 		}
-		if (teamIds != null && !teamIds.isEmpty()) {
-			urlBuilder.addQueryParameter("teamsIds", String.join(",", teamIds));
+		if (params.getTeamIds() != null && !params.getTeamIds().isEmpty()) {
+			urlBuilder.addQueryParameter("teamsIds", String.join(",", params.getTeamIds()));
 		}
-		if (workerStates != null && !workerStates.isEmpty()) {
-			urlBuilder.addQueryParameter("states", workerStates
+		if (params.getWorkerStates() != null && !params.getWorkerStates().isEmpty()) {
+			urlBuilder.addQueryParameter("states", params.getWorkerStates()
 					.stream()
 					.map(Object::toString)
 					.collect(Collectors.joining(",")));
 		}
-		if (phoneNumbers != null && !phoneNumbers.isEmpty()) {
-			urlBuilder.addQueryParameter("phoneNumbers", String.join(",", phoneNumbers));
+		if (params.getPhoneNumbers() != null && !params.getPhoneNumbers().isEmpty()) {
+			urlBuilder.addQueryParameter("phoneNumbers", String.join(",", params.getPhoneNumbers()));
 		}
-		if (includePasswordDetails) {
+		if (params.getIncludePasswordDetails()) {
 			urlBuilder.addQueryParameter("includePasswordDetails", "true");
 		}
 		Response response = sendRequest(HttpMethodType.GET, urlBuilder.toString());
@@ -63,23 +65,23 @@ public class WorkerApi extends BaseApi {
 		}.getType());
 	}
 
-	public Tasks getWorkersTasks(String workerId, Long from, Long to, String lastId, Boolean isPickUpTask) throws ApiException {
+	public WorkerTasks getWorkersTasks(String workerId, WorkerTasksQueryParams params) throws ApiException {
 		String url = String.format("%s/%s/tasks", baseUrl, workerId);
 		HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder();
-		if (from != null)
-			urlBuilder.addQueryParameter("from", from.toString());
-		if (to != null)
-			urlBuilder.addQueryParameter("to", to.toString());
-		if (lastId != null)
-			urlBuilder.addQueryParameter("lastId", lastId);
-		if (isPickUpTask != null)
-			urlBuilder.addQueryParameter("isPickUpTask", isPickUpTask.toString());
+		if (params.getFrom() != null)
+			urlBuilder.addQueryParameter("from", params.getFrom().toString());
+		if (params.getTo() != null)
+			urlBuilder.addQueryParameter("to", params.getTo().toString());
+		if (params.getLastId() != null)
+			urlBuilder.addQueryParameter("lastId", params.getLastId());
+		if (params.getPickUpTask() != null)
+			urlBuilder.addQueryParameter("isPickUpTask", params.getPickUpTask().toString());
 		Response response = sendRequest(HttpMethodType.GET, urlBuilder.build().toString());
-		return handleResponse(response, Tasks.class);
+		return handleResponse(response, WorkerTasks.class);
 	}
 
-	public Tasks getWorkersTasks(String workerId) throws ApiException {
-		return this.getWorkersTasks(workerId, null, null, null, null);
+	public WorkerTasks getWorkersTasks(String workerId) throws ApiException {
+		return this.getWorkersTasks(workerId, null);
 	}
 
 	public Workers getWorkersByLocation(Double longitude, Double latitude, Integer radius) throws ApiException {
@@ -93,25 +95,55 @@ public class WorkerApi extends BaseApi {
 		return handleResponse(response, Workers.class);
 	}
 
-	public Worker getSingleWorker(String workerId, List<WorkerFilterFields> filterFields, Boolean enableAnalytics) throws ApiException {
+
+	/**
+	 * filter - Optional. A comma-separated list of fields to return, if all are not desired. For example, name, location
+	 * phones - Optional. A comma-separated list of workers' phone numbers.
+	 * states - Optional. A comma-separated list of worker states, where 0 is off-duty,
+	 * 1 is idle (on-duty, no active task) and 2 is active (on-duty, active task).
+	 * teams - Optional. A comma-separated list of the team IDs that workers must be part of.
+	 * analytics
+	 */
+	public Worker getSingleWorker(String workerId, WorkerQueryParams params) throws ApiException {
 		String url = String.format("%s/%s", baseUrl, workerId);
 		HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder();
-		if (filterFields != null && !filterFields.isEmpty()) {
-			String filterQuery = filterFields.stream()
+		if (params.getFilterFields() != null && !params.getFilterFields().isEmpty()) {
+			String filterQuery = params.getFilterFields().stream()
 					.map(WorkerFilterFields::getValue)
 					.collect(Collectors.joining(","));
 			urlBuilder.addQueryParameter("filter", filterQuery);
 		}
-		if (enableAnalytics != null) {
-			urlBuilder.addQueryParameter("analytics", enableAnalytics.toString());
+		if (params.getPhones() != null && !params.getPhones().isEmpty()) {
+			urlBuilder.addQueryParameter("phones", String.join(",", params.getPhones()));
+		}
+		if (params.getStates() != null) {
+			urlBuilder.addQueryParameter("states", params.getStates()
+					.stream()
+					.map(Object::toString)
+					.collect(Collectors.joining(",")));
+		}
+		if (params.getTeams() != null) {
+			urlBuilder.addQueryParameter("teams", String.join(",", params.getTeams()));
+		}
+		if (params.getEnableAnalytics() != null) {
+			urlBuilder.addQueryParameter("analytics", params.getEnableAnalytics().toString());
 		}
 		Response response = sendRequest(HttpMethodType.GET, urlBuilder.build().toString());
 		return handleResponse(response, Worker.class);
 	}
 
-	public Worker updateWorker(Worker worker) throws ApiException {
-		String url = String.format("%s/%s", baseUrl, worker.getId());
-		String jsonPayload = GsonSingleton.getInstance().toJson(worker);
+	public List<Worker> queryWithMetadata(List<Metadata> metadata) throws ApiException {
+		String url = String.format("%s/metadata", baseUrl);
+		String jsonPayload = GsonSingleton.getInstance().toJson(metadata);
+		RequestBody body = RequestBody.create(jsonPayload, MediaTypes.JSON);
+		Response response = sendRequest(HttpMethodType.PUT, body, url);
+		return handleResponse(response, new TypeToken<List<Worker>>() {
+		}.getType());
+	}
+
+	public Worker updateWorker(String workerId, WorkerUpdateParams params) throws ApiException {
+		String url = String.format("%s/%s", baseUrl, workerId);
+		String jsonPayload = GsonSingleton.getInstance().toJson(params);
 		RequestBody body = RequestBody.create(jsonPayload, MediaTypes.JSON);
 		Response response = sendRequest(HttpMethodType.PUT, body, url);
 		return handleResponse(response, Worker.class);
@@ -121,17 +153,17 @@ public class WorkerApi extends BaseApi {
 		sendRequest(HttpMethodType.DELETE, String.format("%s/%s", baseUrl, workerId));
 	}
 
-	public ScheduleEntries getWorkerSchedule(String workerId) throws ApiException {
+	public WorkerScheduleEntries getWorkerSchedule(String workerId) throws ApiException {
 		Response response = sendRequest(HttpMethodType.GET, String.format("%s/%s/schedule", baseUrl, workerId));
-		return handleResponse(response, ScheduleEntries.class);
+		return handleResponse(response, WorkerScheduleEntries.class);
 	}
 
-	public ScheduleEntries setWorkerSchedule(String workerId, List<ScheduleEntry> entries) throws ApiException {
+	public WorkerScheduleEntries setWorkerSchedule(String workerId, List<WorkerScheduleEntry> entries) throws ApiException {
 		String url = String.format("%s/%s/schedule", baseUrl, workerId);
 		JsonObject jsonPayload = new JsonObject();
 		JsonArray entriesArray = new JsonArray();
 
-		for (ScheduleEntry entry : entries) {
+		for (WorkerScheduleEntry entry : entries) {
 			JsonObject entryObject = new JsonObject();
 			entryObject.addProperty("date", entry.getDate());
 			entryObject.addProperty("timezone", entry.getTimezone());
@@ -149,7 +181,7 @@ public class WorkerApi extends BaseApi {
 		jsonPayload.add("entries", entriesArray);
 		RequestBody body = RequestBody.create(jsonPayload.toString(), MediaTypes.JSON);
 		Response response = sendRequest(HttpMethodType.POST, body, url);
-		return handleResponse(response, ScheduleEntries.class);
+		return handleResponse(response, WorkerScheduleEntries.class);
 	}
 
 }
