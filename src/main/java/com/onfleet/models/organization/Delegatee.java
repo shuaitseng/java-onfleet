0984544150b11f3cf80e40cbf52f3ba7d106f2c2
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-20 17:56:17 -0300

    fixed Delegatee attributes

--- src/main/java/com/onfleet/models/organization/Delegatee.java
+++ src/main/java/com/onfleet/models/organization/Delegatee.java
@@ -2,29 +2,7 @@ package com.onfleet.models.organization;
 
 
 public class Delegatee extends Organization {
-	private String driverSupportEmail;
-	private String image;
 	private Boolean isFulfillment;
-
-	@Override
-	public String getDriverSupportEmail() {
-		return driverSupportEmail;
-	}
-
-	@Override
-	public void setDriverSupportEmail(String driverSupportEmail) {
-		this.driverSupportEmail = driverSupportEmail;
-	}
-
-	@Override
-	public String getImage() {
-		return image;
-	}
-
-	@Override
-	public void setImage(String image) {
-		this.image = image;
-	}
 
 	public Boolean getFulfillment() {
 		return isFulfillment;
