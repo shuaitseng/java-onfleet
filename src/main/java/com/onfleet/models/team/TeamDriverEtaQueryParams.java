f2e6302550a6a16ac76d2a4a8271e40284bd934b
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-20 16:54:51 -0300

    added builder pattern for team eta

--- src/main/java/com/onfleet/models/team/TeamDriverEtaQueryParams.java
+++ src/main/java/com/onfleet/models/team/TeamDriverEtaQueryParams.java
@@ -9,6 +9,14 @@ public class TeamDriverEtaQueryParams {
 	private VehicleType[] restrictedVehicleTypes;
 	private Long serviceTime;
 
+	private TeamDriverEtaQueryParams(Builder builder) {
+		this.dropoffLocation = builder.dropoffLocation;
+		this.pickupLocation = builder.pickupLocation;
+		this.pickupTime = builder.pickupTime;
+		this.restrictedVehicleTypes = builder.restrictedVehicleTypes;
+		this.serviceTime = builder.serviceTime;
+	}
+
 	public String getDropoffLocation() {
 		return dropoffLocation;
 	}
@@ -47,5 +55,42 @@ public class TeamDriverEtaQueryParams {
 
 	public void setServiceTime(Long serviceTime) {
 		this.serviceTime = serviceTime;
+	}
+
+	public static class Builder {
+		private String dropoffLocation;
+		private String pickupLocation;
+		private Long pickupTime;
+		private VehicleType[] restrictedVehicleTypes;
+		private Long serviceTime;
+
+		public Builder dropoffLocation(String dropoffLocation) {
+			this.dropoffLocation = dropoffLocation;
+			return this;
+		}
+
+		public Builder pickupLocation(String pickupLocation) {
+			this.pickupLocation = pickupLocation;
+			return this;
+		}
+
+		public Builder pickupTime(Long pickupTime) {
+			this.pickupTime = pickupTime;
+			return this;
+		}
+
+		public Builder restrictedVehicleTypes(VehicleType[] restrictedVehicleTypes) {
+			this.restrictedVehicleTypes = restrictedVehicleTypes;
+			return this;
+		}
+
+		public Builder serviceTime(Long serviceTime) {
+			this.serviceTime = serviceTime;
+			return this;
+		}
+
+		public TeamDriverEtaQueryParams build() {
+			return new TeamDriverEtaQueryParams(this);
+		}
 	}
 }
