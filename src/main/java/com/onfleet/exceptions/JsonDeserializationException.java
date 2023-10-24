ff278919e0503e7362c8e60d584dc2277d6a504c
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-24 18:16:25 -0300

    deleted unused classes

--- src/main/java/com/onfleet/exceptions/JsonDeserializationException.java
+++
@@ -1,7 +0,0 @@
-package com.onfleet.exceptions;
-
-public class JsonDeserializationException extends RuntimeException {
-	public JsonDeserializationException(String message, Throwable t) {
-		super(message, t);
-	}
-}
--- src/main/java/com/onfleet/utils/ThrowingSupplier.java
+++
@@ -1,6 +0,0 @@
-package com.onfleet.utils;
-
-@FunctionalInterface
-public interface ThrowingSupplier<T, E extends Exception> {
-	T get() throws E;
-}
