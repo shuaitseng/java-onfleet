78deb91640c2a8ade12def4ffc7e58c7bd6d10a2
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-24 14:55:44 -0300

    added builder for task list query params

--- src/main/java/com/onfleet/models/task/TaskListQueryParams.java
+++ src/main/java/com/onfleet/models/task/TaskListQueryParams.java
@@ -91,4 +91,70 @@ public class TaskListQueryParams {
 	public void setTaskDependencies(List<String> taskDependencies) {
 		this.taskDependencies = taskDependencies;
 	}
+
+	public static class Builder {
+		private final long from;
+		private Long to;
+		private String lastId;
+		private String worker;
+		private Long completeBeforeBefore;
+		private Long completeAfterAfter;
+		private List<TaskState> states;
+		private List<String> taskDependencies;
+
+		public Builder(long from) {
+			this.from = from;
+		}
+
+		public Builder setTo(Long to) {
+			this.to = to;
+			return this;
+		}
+
+		public Builder setLastId(String lastId) {
+			this.lastId = lastId;
+			return this;
+		}
+
+		public Builder setWorker(String worker) {
+			this.worker = worker;
+			return this;
+		}
+
+		public Builder setCompleteBeforeBefore(Long completeBeforeBefore) {
+			this.completeBeforeBefore = completeBeforeBefore;
+			return this;
+		}
+
+		public Builder setCompleteAfterAfter(Long completeAfterAfter) {
+			this.completeAfterAfter = completeAfterAfter;
+			return this;
+		}
+
+		public Builder setStates(List<TaskState> states) {
+			this.states = states;
+			return this;
+		}
+
+		public Builder setTaskDependencies(List<String> taskDependencies) {
+			this.taskDependencies = taskDependencies;
+			return this;
+		}
+
+		public TaskListQueryParams build() {
+			return new TaskListQueryParams(this);
+		}
+	}
+
+	private TaskListQueryParams(Builder builder) {
+		from = builder.from;
+		to = builder.to;
+		lastId = builder.lastId;
+		worker = builder.worker;
+		completeBeforeBefore = builder.completeBeforeBefore;
+		completeAfterAfter = builder.completeAfterAfter;
+		states = builder.states;
+		taskDependencies = builder.taskDependencies;
+	}
+
 }
