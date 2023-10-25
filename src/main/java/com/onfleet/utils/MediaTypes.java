8786729fac17001d563890fb96ca467932f3ef0b
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-25 18:07:11 -0300

    added private constructor

--- src/main/java/com/onfleet/utils/MediaTypes.java
+++ src/main/java/com/onfleet/utils/MediaTypes.java
@@ -3,6 +3,9 @@ package com.onfleet.utils;
 import okhttp3.MediaType;
 
 public class MediaTypes {
+	private MediaTypes() {
+	}
+
 	public static final MediaType JSON = MediaType.parse("application/json");
 	public static final MediaType XML = MediaType.parse("application/xml");
 	public static final MediaType PLAIN_TEXT = MediaType.parse("text/plain");
