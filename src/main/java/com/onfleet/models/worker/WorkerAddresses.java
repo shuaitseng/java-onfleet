0f6beeaf5a09889461d1de20f71aadcce98e95cf
Gonzalo Grisafi <gonzagrisa@gmail.com>
2023-10-26 13:13:43 -0300

    added missing constructor

--- src/main/java/com/onfleet/models/worker/WorkerAddresses.java
+++ src/main/java/com/onfleet/models/worker/WorkerAddresses.java
@@ -3,6 +3,10 @@ package com.onfleet.models.worker;
 public class WorkerAddresses {
 	private WorkerRouting routing;
 
+	public WorkerAddresses(WorkerRouting routing) {
+		this.routing = routing;
+	}
+
 	public WorkerRouting getRouting() {
 		return routing;
 	}
